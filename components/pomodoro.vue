a80dd81145a54912de7dff0099a33773e0f9a6b3
Ae111n <bouguedraaymen@gmail.com>
2024-12-01 16:47:41 +0100

    added the page structure and main functionality with some styling ..

--- components/pomodoro.vue
+++ components/pomodoro.vue
@@ -2,37 +2,89 @@
   <div id="container">
     <h1>25 + 5 Clock</h1>
     <div id="controls">
-      <div class="adjust-length">
-        <h3>Break Length</h3>
-        <button class="arrow">+</button>
-        <span>25:00</span>
-        <button class="arrow">-</button>
-      </div>
+
       <div class="adjust-length">
         <h3>Session Length</h3>
-        <button class="arrow">+</button>
-        <span>25:00</span>
-        <button class="arrow">-</button>
+        <button @click=setCounntdown(25)>25m</button>
+        <button @click=setCounntdown(45)>45m</button>
+
+      </div>
+
+      <div class="adjust-length">
+        <h3>Break Length</h3>
+        <button @click=setCounntdown(15)>15m</button>
+        <button @click=setCounntdown(5)>5m</button>
       </div>
     </div>
 
-    <div id="clock"></div>
+    <div id="clock">{{ displayMinutes }}:<h1 v-show="zero">0</h1>{{displaySeconds }}</div>
+
+    <div class="adjust-length">
+      <h3>controls</h3>
+      <button @click="startCountdown">start</button>
+      <button @click="pauseCountdown">pause</button>
+      <button @click="resetCountdown">reset</button>
+    </div>
   </div>
+
 </template>
 
 <script>
 export default {
   data() {
     return {
+      displayMinutes: 25,
+      displaySeconds: '00',
+      interval: null,
+    audio : new Audio('https://codeskulptor-demos.commondatastorage.googleapis.com/descent/gotitem.mp3')
+,
+zero:false
     }
   },
+
   methods: {
+    setCounntdown(x) {
+      this.displayMinutes = x,
+        this.displaySeconds = '00',
+        clearInterval(this.interval);
+      this.interval = null;
+
+    },
+    updateCountdown() {
+
+
+      if (this.displayMinutes === 0 && this.displaySeconds === 0) {
+        clearInterval(this.interval);
+        this.audio.play()
+        return;
+      }
+
+      if (this.displaySeconds === '00') {
+        this.displayMinutes -= 1;
+        this.displaySeconds = 59;
+      } else {
+        this.displaySeconds -= 1;
+        this.zero=false
+      }
+    },
+    startCountdown() {
+      if (!this.interval) {
+        this.interval = setInterval(this.updateCountdown, 1000);
+      }
+    },
+    pauseCountdown() {
+      clearInterval(this.interval);
+      this.interval = null;
+    },
+    resetCountdown() {
+      clearInterval(this.interval);
+      this.interval = null;
+      this.displayMinutes = 25;
+      this.displaySeconds = '00';
+ 
+    },
 
   }
-
-
-
-
 }
 </script>
 
@@ -42,7 +94,7 @@ export default {
   width: 98vw;
   height: 98vh;
   margin: auto;
-
+  background-color: black;
 }
 
 h1 {
@@ -56,8 +108,7 @@ h1 {
 }
 
 .adjust-length {
-
-  width: 250px;
+  width: 210px;
   height: 140px;
   margin: auto;
   text-align: center;
@@ -65,24 +116,33 @@ h1 {
 }
 
 #controls {
-
   display: flex;
   flex-direction: row;
 }
 
 button {
-
-  width: 40px;
-  height: 40px;
-  border-radius: 20px;
+  width: 100px;
+  height: 50px;
+  border-radius: 5px;
+  font-size: larger;
+  background-color: black;
+  color: lime;
+  box-shadow: rgba(9, 255, 0, 0.612) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
 }
+
 #clock {
   min-width: 500px;
-  max-width :70vw;
-  min-height: 40vh;
-  max-height: 100vh;
-  border: 2px solid purple;
+  max-width: 30vw;
+  min-height: 20vh;
+  max-height: 40vh;
+
   margin: auto;
   margin-top: 30px;
+  font-size: 200px;
+  text-align: center;
+  color: limegreen;
+}
+h3 {
+  color: lime;
 }
 </style>
